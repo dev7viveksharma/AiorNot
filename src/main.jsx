06d2f62201d0de340db6d99dce86026199d87f5b
dev7viveksharma <viveklvs123@gmail.com>
2026-08-20 02:03:31 +0530

    alpha v0.9.3

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from 'axios';
 import App from './App.jsx'
 import AuthProvider from "./AuthProvider.jsx";
 import './index.css';
 
+axios.defaults.baseURL = import.meta.env.PROD
+    ? import.meta.env.VITE_SERVER_URL
+    : "";
+
+axios.defaults.withCredentials = true;
+
 const queryclient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
   <QueryClientProvider client={queryclient}>
     <AuthProvider>
       <App/>
     </AuthProvider>
   </QueryClientProvider>
-  // </StrictMode>, 
+  </StrictMode>, 
 )
